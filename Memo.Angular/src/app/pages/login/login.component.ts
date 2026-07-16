a649b373f1928b547cb9e1cb3bcdaf5678e585f6
Awake1223 <zhora.sviridov.05@bk.ru>
2026-07-16 11:38:30 +0500

    standalone false

--- Memo.Angular/src/app/pages/login/login.component.ts
+++ Memo.Angular/src/app/pages/login/login.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  standalone: false,
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
